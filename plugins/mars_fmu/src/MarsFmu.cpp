4c0f2b690b6b905760270fa502485e195691a965
jmartensen <julius.martensen@dfki.de>
2018-08-30 13:18:21 +0200

    Implement IO Map

--- plugins/mars_fmu/src/MarsFmu.cpp
+++ plugins/mars_fmu/src/MarsFmu.cpp
@@ -31,6 +31,10 @@
 #include <mars/data_broker/DataBrokerInterface.h>
 #include <mars/data_broker/DataPackage.h>
 
+#include <mars/interfaces/sim/MotorManagerInterface.h>
+#include <mars/interfaces/sim/SensorManagerInterface.h>
+
+// External libraries from FMI Lib & JModelica
 #include <fmilib.h>
 #include <JM/jm_portability.h>
 
@@ -55,13 +59,35 @@ namespace mars {
 
       void MarsFmu::init() {
 
+        // Data broker configuration
+        typeName = "FMU";
+        data_broker::DataPackage dbPackage;
+
+        // Get the mars nodes
+        int i = 0;
+
+        if(!input_nodes[0].empty()){
+          for(i = 0; i < sizeof(input_nodes)/sizeof(input_nodes[0]); i++){
+            mars_input_ids[i] = control->motors->getID(input_nodes[i]);
+            printf("Found Motor %s : %d \n",input_nodes[i].c_str(), mars_input_ids[i] );
+          }
+        }
+
+        if(!output_nodes[0].empty()){
+          for(i = 0; i < sizeof(output_nodes)/sizeof(output_nodes[0]); i++){
+            mars_output_ids[i] = control->sensors->getSensorID(output_nodes[i]);
+            printf("Found Sensor %s : %d \n",output_nodes[i].c_str(), mars_output_ids[i] );
+          }
+        }
+
+
         // Setup the callbacks
         callbacks.malloc = malloc;
         callbacks.calloc = calloc;
         callbacks.realloc = realloc;
         callbacks.free = free;
         callbacks.logger = importlogger;
-        callbacks.log_level = jm_log_level_debug;
+        callbacks.log_level = jm_log_level_warning;
         callbacks.context = 0;
 
         // Get context and version
@@ -69,50 +95,205 @@ namespace mars {
         // Need to give c strings
         version = fmi_import_get_fmi_version(context, fmu_path.c_str(), tmp_path.c_str());
 
+        // Check for version of the FMU
         if(version != fmi_version_2_0_enu) {
           printf("The code only supports version 2.0\n");
         }
 
+        // Read the XML
         fmu = fmi2_import_parse_xml(context, tmp_path.c_str(), 0);
+
+
+        // Get the value Reference for in and outputs
+        if(!fmu_inputs[0].empty()){
+          for(i = 0; i < sizeof(fmu_inputs)/sizeof(fmu_inputs[0]); i++){
+            // Find the value reference
+            fmi2_import_variable_t* vr_pointer = fmi2_import_get_variable_by_name(fmu, fmu_inputs[i].c_str() );
+            fmu_Input_Ref[i] = fmi2_import_get_variable_vr(vr_pointer);
+            // Print for debug
+            printf("Inputs : %d = %s , VR : %d \n" , i, fmu_inputs[i].c_str(), fmu_Input_Ref[i] );
+            // Add the data broker packages
+            dbPackage.add("Inputs/"+fmu_inputs[i], 0.0);
+            // Create the ID mapping
+
+          }
+        }
+
+        // Get the MARS -> FMU map
+        for(auto mapping : mars_fmu_map){
+          // Find the mars sensors
+          fmi2_import_variable_t* vr_pointer = fmi2_import_get_variable_by_name(fmu, mapping.second.c_str() );
+          mars_fmu_ID[control->motors->getID(mapping.first)] = fmi2_import_get_variable_vr(vr_pointer);
+        }
+
+        for(auto mapping : mars_fmu_ID){
+          printf("%d : %d \n", mapping.first, mapping.second );
+        }
+
+        if(!fmu_outputs[0].empty()){
+          for(i = 0; i < sizeof(fmu_outputs)/sizeof(fmu_outputs[0]); i++){
+            // Find the value reference
+            fmi2_import_variable_t* vr_pointer = fmi2_import_get_variable_by_name(fmu, fmu_outputs[i].c_str() );
+            fmu_Output_Ref[i] = fmi2_import_get_variable_vr(vr_pointer);
+            // Print for debug
+            printf("Outputs : %d = %s, VR : %d \n", i, fmu_outputs[i].c_str(), fmu_Output_Ref[i] );
+            // Add the data broker packages
+            dbPackage.add("Outputs/"+fmu_outputs[i], 0.0);
+          }
+        }
 
         if(!fmu) {
       		printf("Error parsing XML, exiting\n");
       	}
 
+        // Check for FMU type (Cosim or ModelExchange)
         if(fmi2_import_get_fmu_kind(fmu) == fmi2_fmu_kind_me) {
       		printf("Only CS 2.0 is supported by this code\n");
       	}
 
-        callbacks.logger = fmi2_log_forwarding;
-      	callbacks.allocateMemory = calloc;
-      	callbacks.freeMemory = free;
-      	callbacks.componentEnvironment = fmu;
+        // Set callBackFunctions
+        callBackFunctions.logger = fmi2_log_forwarding;
+      	callBackFunctions.allocateMemory = calloc;
+      	callBackFunctions.freeMemory = free;
+      	callBackFunctions.componentEnvironment = fmu;
 
-        status = fmi2_import_create_dllfmu(fmu, fmi2_fmu_kind_cs, &callbacks);
-      	if (status == jm_status_error) {
+        // Check the status
+        fmu_status_jm = fmi2_import_create_dllfmu(fmu, fmi2_fmu_kind_cs, &callBackFunctions);
+      	if (fmu_status_jm == jm_status_error) {
       		printf("Could not create the DLL loading mechanism(C-API) (error: %s).\n", fmi2_import_get_last_error(fmu));
       	}
 
-        printf("Now the simulation should start");
+        // Get a model id
+        fmu_GUID = fmi2_import_get_GUID(fmu);
+
+        // Instanciate the fmu
+        fmu_status_jm = fmi2_import_instantiate(fmu, fmu_instanceName, fmi2_cosimulation, fmu_location, fmu_visible);
+        if(fmu_status_jm == jm_status_error){
+          printf("fmi2_import_instantiate failed! \n");
+          return;
+        }
+
+        // Initialize with the step size as an end time!
+        fmu_status = fmi2_import_setup_experiment(fmu, fmi2_true, fmu_relativeTolerance, current_time, stop_time_defined, time_step);
+        if(fmu_status != fmi2_status_ok){
+          printf("Setup experiment failed! \n");
+        }
+
+        // Initialize the fmu
+        fmu_status = fmi2_import_enter_initialization_mode(fmu);
+        if(fmu_status != fmi2_status_ok){
+          printf("Enter initialization failed! \n");
+        }
+
+        // Exit initialization mode
+        fmu_status = fmi2_import_exit_initialization_mode(fmu);
+        if(fmu_status != fmi2_status_ok){
+          printf("Enter initialization failed! \n");
+        }
+
+        // Make the ID Mapping
+
+
+        // Finish data broker config
+
+        std::string groupName = "external_sim";
+        std::string dataName = "FMU/";
+
+        control->dataBroker->pushData(groupName, dataName, dbPackage, NULL, data_broker::DATA_PACKAGE_READ_FLAG);
+        control->dataBroker->registerTimedProducer(this, groupName, dataName, "mars_sim/simTimer", 0);
 
 
       }
 
       void MarsFmu::reset() {
+        // Reset the fmu and the internal variables
+        fmu_status = fmi2_import_reset(fmu);
+        if(fmu_status != fmi2_status_ok){
+          printf("Reset failed! \n");
+        }
+
+        // Initialize the fmu
+        fmu_status = fmi2_import_enter_initialization_mode(fmu);
+        if(fmu_status != fmi2_status_ok){
+          printf("Enter initialization failed! \n");
+        }
+
+        // Exit initialization mode
+        fmu_status = fmi2_import_exit_initialization_mode(fmu);
+        if(fmu_status != fmi2_status_ok){
+          printf("Enter initialization failed! \n");
+        }
+        current_time = 0.0;
+
       }
 
       MarsFmu::~MarsFmu() {
+        // Remove from dataBroker
+        std::string groupName = "external_sim";
+        std::string dataName = "FMU/";
+        control->dataBroker->unregisterTimedProducer(this, groupName, dataName, "mars_sim/simTimer");
+
+        // Destroy the fmu
+        fmi2_import_destroy_dllfmu(fmu);
+        fmi2_import_free(fmu);
+        fmi_import_free_context(context);
+        printf("Destroyed fmu! \n");
       }
 
 
       void MarsFmu::update(sReal time_ms) {
+        // Get the input values
+        sReal *sensorData;
+        int numSensorValues = control->sensors->getSensorData(mars_output_ids[0], &sensorData);
+        fmu_input_values[1] = sensorData[0];
+        //printf("%d \n", numSensorValues);
+        free(sensorData);
+        // Conversion needed because mars calculates in ms
+        while(current_time*1000 < time_ms){
+          fmu_status = fmi2_import_set_real(fmu, fmu_Input_Ref, 2, fmu_input_values);
+          // Do a step
+          fmu_status = fmi2_import_do_step(fmu, current_time, time_step, fmi2_true);
+          // Get results
+          //fmi2_real_t values[2];
+          fmu_status = fmi2_import_get_real(fmu, fmu_Output_Ref, 1, fmu_output_values);
+          // printf("y[0] = %2.4f \n y[1] = %2.4f", fmu_results[0], fmu_results[1]);
+          // Check if fmu feels good
+          if(fmu_status != fmi2_status_ok){
+            printf("Error during simulation! \n");
+          }
+          // Raise time
+          current_time += time_step;
+        }
+        // Set the motor value
+        control->motors->setMotorValue(mars_input_ids[0], fmu_output_values[0]);
 
+        // Reset internal time
+        current_time = 0.0;
         // control->motors->setMotorValue(id, value);
       }
 
-      void MarsFmu::receiveData(const data_broker::DataInfo& info,
-                                    const data_broker::DataPackage& package,
-                                    int id) {
+      void MarsFmu::produceData(const data_broker::DataInfo &info,
+                                         data_broker::DataPackage *package,
+                                         int callbackParam) {
+          int i = 0;
+          if(!fmu_inputs[0].empty()){
+            for(i = 0; i < sizeof(fmu_inputs)/sizeof(fmu_inputs[0]); i++){
+              // Send to the data broker
+              package->set(i, fmu_input_values[i]);
+            }
+          }
+
+          int j = i;
+            if(!fmu_outputs[0].empty()){
+             for(i = 0; i < sizeof(fmu_outputs)/sizeof(fmu_outputs[0]); i++){
+               // Send to the data broker
+               package->set(i+j, fmu_output_values[i]);
+             }
+            }
+
+                                    }
+
+      void MarsFmu::receiveData(const data_broker::DataInfo &info, const data_broker::DataPackage &package, int callbackParam){
         // package.get("force1/x", force);
       }
 
--- plugins/mars_fmu/src/MarsFmu.h
+++ plugins/mars_fmu/src/MarsFmu.h
@@ -37,8 +37,15 @@
 //#define PLUGIN_WITH_MARS_GUI
 #include <mars/interfaces/sim/MarsPluginTemplate.h>
 #include <mars/interfaces/MARSDefs.h>
-#include <mars/data_broker/ReceiverInterface.h>
+
 #include <mars/cfg_manager/CFGManagerInterface.h>
+
+// Use to publish the results and get the forces
+#include <mars/data_broker/ProducerInterface.h>
+#include <mars/data_broker/ReceiverInterface.h>
+
+#include <mars/interfaces/sim/MotorManagerInterface.h>
+#include <mars/interfaces/sim/SensorManagerInterface.h>
 
 #include <string>
 
@@ -51,7 +58,9 @@ namespace mars {
 
       // inherit from MarsPluginTemplateGUI for extending the gui
       class MarsFmu: public mars::interfaces::MarsPluginTemplate,
-        public mars::data_broker::ReceiverInterface,
+      mars::data_broker::ReceiverInterface,
+      mars::data_broker::ProducerInterface,
+
         // for gui
         // public mars::main_gui::MenuInterface,
         public mars::cfg_manager::CFGClient {
@@ -73,8 +82,9 @@ namespace mars {
         void update(mars::interfaces::sReal time_ms);
 
         // DataBrokerReceiver methods
-        virtual void receiveData(const data_broker::DataInfo &info,
-                                 const data_broker::DataPackage &package,
+        virtual void receiveData(const data_broker::DataInfo &info,const data_broker::DataPackage &package, int callbackParam);
+        virtual void produceData(const data_broker::DataInfo &info,
+                                 data_broker::DataPackage *package,
                                  int callbackParam);
         // CFGClient methods
         virtual void cfgUpdateProperty(cfg_manager::cfgPropertyStruct _property);
@@ -84,21 +94,69 @@ namespace mars {
 
         // MarsFmu methods
 
+
+
+
       private:
         cfg_manager::cfgPropertyStruct example;
+
+        // Inputs and Outputs for the FMU
+        std::string fmu_inputs[2] = {"input_voltage", "external_torque"};
+        std::string fmu_outputs[1] = {"output_speed"};
+
+        // Mars Inputs and outputs
+        std::string input_nodes[1] = {"motor_a"};
+        std::string output_nodes[1] = {"joint_torque"};
+
+        // Map inputs and outputs from fmu to mars and vice versa
+        std::map<std::string, std::string> mars_fmu_map = { {"joint_torque", "external_torque"}};
+        std::map<std::string, std::string> fmu_mars_map = { {"output_speed", "motor_a"} };
+
+        // Make some ID maps
+        std::map<unsigned long, unsigned long> mars_fmu_ID;
+        std::map<unsigned long, unsigned long> fmu_mars_ID;
+
         // File system
-        std::string fmu_path = "/media/jmartensen/Data/linux/mars_dev/simulation/mars/plugins/mars_fmu/Test/BouncingBall2.fmu";
+        std::string typeName;
+        std::string fmu_path = "/media/jmartensen/Data/linux/mars_dev/simulation/mars/plugins/mars_fmu/Test/PT2.fmu";
         std::string tmp_path = "/media/jmartensen/Data/linux/mars_dev/simulation/mars/plugins/mars_fmu/tmp";
 
         // Callbacks
         jm_callbacks callbacks;
 
-        // FMI
+        // FMI - Global variables used for managing
         fmi_import_context_t* context;
         fmi_version_enu_t version;
-        jm_status_enu_t status;
 
         fmi2_import_t* fmu;
+        fmi2_callback_functions_t callBackFunctions;
+
+        // FMI - Global variables used for simulation
+        fmi2_string_t fmu_instanceName = "Test Model";
+        fmi2_string_t fmu_GUID;
+        fmi2_string_t fmu_location = "";
+        fmi2_boolean_t fmu_visible = fmi2_false;
+        fmi2_real_t fmu_relativeTolerance = 1e-2;
+
+        fmi2_status_t fmu_status;
+        jm_status_enu_t fmu_status_jm;
+
+        // Need to work on that, here are the outputs
+        fmi2_value_reference_t fmu_Input_Ref[2] = {0, 0};
+        fmi2_value_reference_t fmu_Output_Ref[1] = {0};
+
+        // Store inputs and outputs
+        fmi2_real_t fmu_input_values[2] = {22.0, 10.0};
+        fmi2_real_t fmu_output_values[1] = {0.0};
+
+        fmi2_real_t current_time = 0.0;
+        fmi2_real_t time_step = 1e-3;
+        fmi2_boolean_t stop_time_defined = fmi2_false;
+
+        // Mars IDs
+        unsigned long mars_input_ids[1] = {0};
+        unsigned long mars_output_ids[1] = {0};
+
 
       }; // end of class definition MarsFmu
 
